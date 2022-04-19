c0fa73e8263e4b1741ec6b607c480f595b8b11b5
WilliamThomsonNZ <william__thomson@outlook.com>
2022-04-19 16:30:29 +1200

    Refactor staking contract for easier reading of users tokens

--- contracts/GameStaking.sol
+++ contracts/GameStaking.sol
@@ -4,82 +4,130 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 import "./GameToken.sol";
+import "hardhat/console.sol";
 
 contract ItemStake {
     IERC721 public gameItem;
     GameToken token;
-    struct StakedItem {
-        uint256 tokenId;
-        uint256 timestamp;
-        address owner;
+
+    struct Staker {
+        uint256[] stakedTokens;
+        mapping(uint256 => uint256) tokenIndex;
+        uint256 lastUpdate;
+        uint256 tokenBalance;
     }
 
-    event NFTStaked(address owner, uint256 tokenId, uint256 value);
-    event NFTUnstaked(address owner, uint256 tokenId, uint256 value);
+    event NFTStaked(address owner, uint256 tokenId);
+    event NFTUnstaked(address owner, uint256 tokenId);
 
-    mapping(uint256 => StakedItem) public stakedItems;
-    uint256 stakedTokenAmount;
+    //mapping(uint256 => s) public stakedItems;
+    mapping(address => Staker) public stakers;
+    mapping(uint256 => address) public tokenOwners;
 
     constructor(address _gameToken, address _gameItem) {
         gameItem = IERC721(_gameItem);
         token = GameToken(_gameToken);
     }
 
-    function stake(uint32[] calldata _tokenIds) external {
-        uint256 tokenId;
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            tokenId = _tokenIds[i];
-            require(
-                gameItem.ownerOf(tokenId) == msg.sender,
-                "Not owner of token"
-            );
-            require(stakedItems[tokenId].tokenId == 0, "Token already staked");
-            gameItem.safeTransferFrom(msg.sender, address(this), tokenId);
-            emit NFTStaked(msg.sender, tokenId, block.timestamp);
-            stakedItems[tokenId] = StakedItem({
-                tokenId: uint32(tokenId),
-                owner: msg.sender,
-                timestamp: block.timestamp
-            });
-            stakedTokenAmount++;
-        }
-    }
-
-    function unstake(uint32[] calldata _tokenIds) public {
-        uint256 tokenId;
-        uint256 tokensToClaim;
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            tokenId = _tokenIds[i];
-            StakedItem memory item = stakedItems[tokenId];
-            require(item.owner == msg.sender, "Not owner of token");
-            //864 for 100 tokens
-            tokensToClaim += (block.timestamp - item.timestamp) / 1;
-            console.log(tokensToClaim);
-            gameItem.safeTransferFrom(address(this), msg.sender, tokenId);
-            emit NFTUnstaked(msg.sender, tokenId, item.timestamp);
-            delete stakedItems[tokenId];
-            stakedTokenAmount--;
-        }
+    function calculateRewards(address _user) internal returns (uint256) {
+        Staker storage staker = stakers[_user];
+        uint256 claimableAmountPerToken = (block.timestamp -
+            staker.lastUpdate) / 1;
+        uint256 tokensToClaim = staker.stakedTokens.length *
+            claimableAmountPerToken;
         if (tokensToClaim > 0) {
-            token.mint(msg.sender, tokensToClaim);
+            staker.tokenBalance += tokensToClaim;
+            return tokensToClaim;
+        }
+        return 0;
+    }
+
+    function claimTokens() external {
+        Staker storage staker = stakers[msg.sender];
+        uint256 claimableTokens = calculateRewards(msg.sender);
+        if (staker.tokenBalance > 0) {
+            staker.tokenBalance = 0;
+            staker.lastUpdate = block.timestamp;
+            token.mint(msg.sender, claimableTokens);
         }
     }
 
-    function getStakedTokens(address _account)
-        public
-        view
-        returns (uint32[] memory)
-    {
-        uint32[] memory arr = new uint32[](stakedTokenAmount);
-        for (uint256 i = 0; i < stakedTokenAmount; i++) {
-            StakedItem memory item = stakedItems[i];
-            if (item.owner == _account) {
-                arr[i] = uint32(item.tokenId);
+    function stakeMultiple(uint256[] calldata _tokenIds) external {
+        uint256 tokenId;
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            tokenId = _tokenIds[i];
+            if (gameItem.ownerOf(tokenId) == msg.sender) {
+                _stake(tokenId, msg.sender);
             }
         }
-        return arr;
+    }
+
+    function stake(uint256 _tokenId) external {
+        require(gameItem.ownerOf(_tokenId) == msg.sender, "Not owner of token");
+        _stake(_tokenId, msg.sender);
+    }
+
+    function _stake(uint256 _tokenId, address _user) internal {
+        Staker storage staker = stakers[_user];
+
+        require(staker.tokenIndex[_tokenId] == 0, "Token already staked");
+        if (staker.stakedTokens.length > 1) {
+            calculateRewards(_user);
+        }
+        staker.stakedTokens.push(_tokenId);
+        staker.tokenIndex[staker.stakedTokens.length - 1];
+        staker.lastUpdate = block.timestamp;
+
+        gameItem.safeTransferFrom(msg.sender, address(this), _tokenId);
+        tokenOwners[_tokenId] = _user;
+        emit NFTStaked(_user, _tokenId);
+    }
+
+    function _unstake(uint256 _tokenId, address _user) internal {
+        Staker storage staker = stakers[_user];
+
+        uint256 lastIndex = staker.stakedTokens.length - 1;
+        uint256 lastIndexKey = staker.stakedTokens[lastIndex];
+        uint256 tokenIdIndex = staker.tokenIndex[_tokenId];
+
+        staker.stakedTokens[tokenIdIndex] = lastIndexKey;
+        //staker.tokenIndex[lastIndexKey] = tokenIdIndex;
+
+        if (staker.stakedTokens.length > 0) {
+            staker.stakedTokens.pop();
+            delete staker.tokenIndex[_tokenId];
+        }
+        if (staker.stakedTokens.length < 0) {
+            delete stakers[_user];
+        }
+        delete tokenOwners[_tokenId];
+
+        gameItem.safeTransferFrom(address(this), _user, _tokenId);
+
+        emit NFTUnstaked(_user, _tokenId);
+    }
+
+    function unstake(uint256 _tokenId) external {
+        require(msg.sender == tokenOwners[_tokenId], "Not token owner");
+        _unstake(_tokenId, msg.sender);
+    }
+
+    function unstakeMultiple(uint256[] calldata _tokenIds) external {
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            if (tokenOwners[_tokenIds[i]] == msg.sender) {
+                _unstake(_tokenIds[i], msg.sender);
+            }
+        }
+    }
+
+    function getStakedTokens(address _user)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        Staker storage staker = stakers[_user];
+        return staker.stakedTokens;
     }
 
     function onERC721Received(
